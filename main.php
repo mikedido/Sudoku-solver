63d1764eda2771e5384b4c59ba523689dcfec368
mahdi gueffaz <mahdi.gueffaz@gmail.com>
2018-12-09 23:39:09 +0100

    Function structure

---
+++ main.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/**
+ * Function to check if the number is absent on the grilled line
+ *
+ *
+ */
+function isAbsentLine()
+{
+
+}
+
+/*
+ * Function to check if the number is absent in the grilled column
+ *
+ *
+ */
+function isAbsentColumn()
+{
+}
+
+
+/**
+ * Function to check if the number is absent in the grilled bloc
+ *
+ *
+ */
+function isAbsentBloc()
+{
+}
+
+/**
+ * Function to resolved sudoku grilled
+ *
+ */
+function isResolved()
+{
+
+}

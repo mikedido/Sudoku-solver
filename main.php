5ac1fe051ec1ccb2fc81e228186369e97a6ea026
Mahdi Gueffaz <mahdi.gueffaz@external.grdf.fr>
2018-12-10 11:09:38 +0100

    Add code on isAbsetColumn and isAbsentLine functions

--- main.php
+++ main.php
@@ -6,9 +6,15 @@
  *
  *
  */
-function isAbsentLine()
+function isAbsentLine(value, array grilled, line)
 {
+    for (j=0; j<9; j++) {
+        if (value == grilled[line][j]) {
+            return false
+        }
+    }
 
+    return true;
 }
 
 /*
@@ -16,25 +22,32 @@ function isAbsentLine()
  *
  *
  */
-function isAbsentColumn()
+function isAbsentColumn(value, array grilled, column)
 {
-}
+    for (i=0; i<9; i++) {
+        if (value == grilled[i][column]) {
+            return false;
+        }
+    }
 
+    return true;
+}
 
 /**
  * Function to check if the number is absent in the grilled bloc
  *
  *
  */
-function isAbsentBloc()
+function isAbsentBloc(value, grilled, line, column)
 {
+    
 }
 
 /**
  * Function to resolved sudoku grilled
  *
  */
-function isResolved()
+function isResolved(value, grilled, position)
 {
 
 }

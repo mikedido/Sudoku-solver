664a1d7074ba601e3af97a9ec72b2e96f2653eb1
Mahdi Gueffaz <mahdigueffaz@gmail.com>
2022-03-02 16:20:18 +0100

    Add jest test

---
+++ __tests__/sudoku.test.js
@@ -0,0 +1,22 @@
+const { expect } = require('@jest/globals');
+const SudokuPuzzle = require('../src/SudokuPuzzle')
+
+describe('', () => {
+    it('Soduku solver easy', () => {
+        let grid = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
+        let sudoku = new SudokuPuzzle(grid)
+
+        expect(sudoku.solve(0,0)).toBeTruthy;
+    })
+
+})
